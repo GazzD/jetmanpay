8777fd725e0910de0d913a8f28ca4c8c1856e32d
GazzD <vcardozof@gmail.com>
2020-03-30 16:13:42 +0200

    Adds password recovery to login form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="login-box">
     <div class="login-logo">
-      <a href="../../index2.html">Jetman Pay</a>
+      <a href="{{route('login')}}">Jetman Pay</a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
@@ -52,24 +52,10 @@
             <!-- /.col -->
           </div>
         </form>
-
-        {{-- <div class="social-auth-links text-center mb-3">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-primary">
-            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-          </a>
-          <a href="#" class="btn btn-block btn-danger">
-            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-          </a>
-        </div>
-        <!-- /.social-auth-links --> --}}
-
-        {{-- <p class="mb-1">
-          <a href="forgot-password.html">I forgot my password</a>
+        
+        <p class="mb-1">
+          <a href="{{ route('password.request') }}">I forgot my password</a>
         </p>
-        <p class="mb-0">
-          <a href="register.html" class="text-center">Register a new membership</a>
-        </p> --}}
       </div>
       <!-- /.login-card-body -->
     </div>
