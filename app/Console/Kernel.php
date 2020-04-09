1f29d480c638e7ae89b48c60ca5f044daf535ce2
GazzD <vcardozof@gmail.com>
2020-04-09 11:54:00 +0200

    Adds dosa loader command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use GuzzleHttp\Client;
+use App\Dosa;
+use app\ScheduleTasks\DosaLoader;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('load:dosas')->everyMinute();
     }
 
     /**
