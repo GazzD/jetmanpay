82dca142efc3ea4b68dfd8e5143792fb41b3f6c9
GazzD <vcardozof@gmail.com>
2020-04-30 18:49:31 +0200

    Fixes visual and translations on edit-profile view

---
+++ resources/lang/en/pages/users.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    
+    /*
+     |--------------------------------------------------------------------------
+     | Users Language Lines
+     |--------------------------------------------------------------------------
+     |
+     | The following language lines are all internal messages for various
+     | messages that we need to display to the user. You are free to modify
+     | these language lines according to your application's requirements.
+     |
+     */
+    
+    'name'                  => 'Name',
+    'phone'                 => 'Phone',
+    'country'               => 'Country',
+    'state'                 => 'State',
+    'zip_code'              => 'ZIP Code',
+    'address_line1'         => 'Address line (1/2)',
+    'address_line2'         => 'Address line (2/2)',
+    'users'                 => 'Users',
+    'profile'               => 'Profile',
+    'edit-profile'          => 'Edit Profile',
+    'email'                 => 'Email',
+    'role'                  => 'Role',
+    'create'                => 'Create',
+    'save'                  => 'Save',
+    'edit'                  => 'Edit',
+    'edit-password'         => 'Edit Password',
+    'change-password'       => 'Change Password',
+    'password'              => 'Password',
+    'password-confirmation' => 'Password confirmation',
+    'back'                  => 'Back',
+];
---
+++ resources/lang/es/pages/users.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    
+    /*
+     |--------------------------------------------------------------------------
+     | Users Language Lines
+     |--------------------------------------------------------------------------
+     |
+     | The following language lines are all internal messages for various
+     | messages that we need to display to the user. You are free to modify
+     | these language lines according to your application's requirements.
+     |
+     */
+    'name'                  => 'Nombre',
+    'phone'                 => 'Teléfono',
+    'country'               => 'País',
+    'state'                 => 'Estado',
+    'zip_code'              => 'Código ZIP',
+    'address_line1'         => 'Dirección (1/2)',
+    'address_line2'         => 'Dirección (2/2)',
+    'users'                 => 'Usuarios',
+    'profile'               => 'Perfil',
+    'edit-profile'          => 'Editar Perfil',
+    'email'                 => 'Correo',
+    'role'                  => 'Rol',
+    'create'                => 'Crear',
+    'save'                  => 'Guardar',
+    'edit'                  => 'Editar',
+    'edit-password'         => 'Editar Contraseña',
+    'change-password'       => 'Cambiar Contraseña',
+    'password'              => 'Contraseña',
+    'password-confirmation' => 'Confirma tu contraseña',
+    'back'                  => 'Volver',
+];
