a413728ddc6c0f65c68360c2845ea9268f6d708a
GazzD <vcardozof@gmail.com>
2020-05-05 17:24:07 +0200

    Fixes payment documents view

--- resources/views/pages/backend/payment-documents/documents.blade.php
+++ resources/views/pages/backend/payment-documents/documents.blade.php
@@ -8,14 +8,14 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">@lang('messages.payment-documents.documents')</h1>
+                <h1 class="m-0 text-dark">@lang('pages/documents.documents')</h1>
                 {{$payment->description}}
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{route('dashboard')}}">@lang('messages.home')</a></li>
-                    <li class="breadcrumb-item"><a href="{{route('pending-payments')}}">@lang('messages.payments.payments')</a></li>
-                    <li class="breadcrumb-item active">@lang('messages.payment-documents.documents')</li>
+                    <li class="breadcrumb-item"><a href="{{route('payments/pending')}}">@lang('messages.payments.payments')</a></li>
+                    <li class="breadcrumb-item active">@lang('pages/documents.documents')</li>
                 </ol>
             </div>
         </div>
@@ -27,6 +27,7 @@
 <section class="content">
     <div class="container-fluid">
         <div class="row">
+        	@if(count($documents))
             @foreach($documents as $document)
             <div class="col-md-3 col-sm-6 col-12">
                 <a href="{{ $document->url }}" target="_blank">
@@ -45,6 +46,11 @@
                 </a>
             </div>
             @endforeach
+            @else
+            <div class="col-12">
+                @lang('pages/documents.empty')
+            </div>
+            @endif
         </div>
     </div>
 </section>
