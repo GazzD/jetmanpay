91135d4e665dcf9d56561a4d326f074946039ad5
GazzD <vcardozof@gmail.com>
2020-07-17 21:12:14 +0200

    Fixes database seeders (adds dosa items generation)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsTableSeeder::class);
         $this->call(PlanesTableSeeder::class);
         $this->call(PaymentsTableSeeder::class);
-//         $this->call(DosasTableSeeder::class);
+        $this->call(DosasTableSeeder::class);
         $this->call(SystemTableSeeder::class);
     }
 }
